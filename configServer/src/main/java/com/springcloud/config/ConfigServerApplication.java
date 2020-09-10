b399e0d23edfa70bc1b638127fef3be05f658eee
jackson-zqf <zql18519704016@163.com>
2020-09-10 18:02:50 +0800

    commit

---
+++ configServer/src/main/java/com/springcloud/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.springcloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ *  @Description:  配置中心启动类
+ *  @author: zqf
+ *  @Date: 2020/9/10 16:23
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
